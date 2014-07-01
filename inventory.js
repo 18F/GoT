4ec2e1580bf55ee2138fe7fbf58fa25d82878148
Kaitlin Devine <kaitlin.devine@gsa.gov>
2014-07-01 14:29:43 -0400

    start of js

---
+++ inventory.js
@@ -0,0 +1,41 @@
+
+var tywin = { 
+              inventory: {
+                swords: 3
+              } 
+            }   
+ var shay = { 
+              inventory: {
+                dragons: 2
+              }
+            }
+ var eddard = { 
+              inventory: {
+                'tears of lys': 7
+              }
+            }
+ var danny = { 
+              inventory: {
+                ship: 1
+              }
+            }
+ 
+
+trades = [];
+
+players = [tywin, shay, eddard, danny]
+
+/* example trade object
+{
+  player1: player1,
+  player2: player2,
+  player1_giving: item,
+  player2_giving: item,
+  accepted: true
+}
+*/
+
+var update_inventory = function (player, giving, receiving) {
+        
+
+}
